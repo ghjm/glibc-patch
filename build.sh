088bd12a8c5a5102ae262e3293a40ea8cac07d66
ghjm <graham@mhn.org>
2018-12-18 14:37:51 -0500

    Print out what's going on

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Get version of latest glibc
+echo Get version of latest glibc...
 VERSION=$(dnf --disablerepo=* --enablerepo=fedora,updates list --available glibc.x86_64 | grep fc29 | awk '{print $2}')
 
-# Make some folders
+echo Make some folders...
 rm -rf rpmbuild
 mkdir rpmbuild
 cd rpmbuild
@@ -12,21 +12,20 @@ mkdir SOURCES
 mkdir SPECS
 cd SRPMS
 
-# Download it
 echo Downloading glibc srpm version $VERSION
 wget https://dl.fedoraproject.org/pub/fedora/linux/updates/29/Everything/SRPMS/Packages/g/glibc-$VERSION.src.rpm
 
-# Unpack it
+echo Unpack the srpm...
 cd ../SOURCES
 rpm2cpio ../SRPMS/glibc-$VERSION.src.rpm | cpio -idmv
 mv glibc.spec ../SPECS
 cd ..
 
-# Update it
+echo Apply the patch...
 cp ../glibc-Revert-elf-Correct-absolute-SHN_ABS-symbol-run-time.patch SOURCES
 ../process-spec-file.py
 
-# Build the SRPM
+echo Build the SRPM...
 TOPDIR=$(pwd)
 rpmbuild --define "_topdir $TOPDIR" -bs SPECS/glibc.spec
 
